a5345c9ee709c96dc0d8950c9fa0d6fa98f808ab
KronembergerYgor <ygorkronemnerger@gmail.com>
2022-06-24 16:46:19 -0300

    Inicio ladding pag

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>AdmEnterprise - Home</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('site/css/style.css') }}" type="text/css">
+        
+    </head>
+
+
+    <body>
+      
+        <nav class="menu">
+
+            <div class="Logo">
+                <span>Logo</span>
+            </div>
+
+            <ul>
+                
+                <li><b><a href="#Home">Home</a></b></li>
+                <li><b><a href="#SobreNos">Sobre Nós</a></b></li>
+                <li><b><a href="#Servicos">Serviços</a></b></li>
+                <li><b><a href="#Contato">Contato</a></b></li>
+
+            </ul>
+
+        </nav>
+
+        <section>
+            <h1>Nome da Empresa</h1>
+            <h2>Titulo</h2>
+
+        </section>
+
+    </body>
+</html>
